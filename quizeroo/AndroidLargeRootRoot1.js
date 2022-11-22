1dfa039f4247dc19ed0facf646e43ac96f29cde4
rucsandrastefan <rucsandra.stefan@student.upt.ro>
2022-11-22 21:27:18 +0200

    high

---
+++ quizeroo/AndroidLargeRootRoot1.js
@@ -0,0 +1,357 @@
+import React from "react";
+import styled from "styled-components";
+
+export const AndroidLargeRootRoot1 = ({}) => {
+  return (
+    <AndroidLargeRootRootRoot>
+      <FlexRow>
+        <Ellipse2>
+          <Gamepadlight>
+            <Image1 src={`https://file.rendit.io/n/hRndQNvwFvxWvL7a5mCp.svg`} />
+            <FlexRow1>
+              <FlexColumn>
+                <Image2
+                  src={`https://file.rendit.io/n/Un7yaCtS3jFRcaUSLlSW.svg`}
+                />
+                <Image3
+                  src={`https://file.rendit.io/n/TL6joErqZM6ndfYIBAjZ.svg`}
+                />
+              </FlexColumn>
+              <FlexRow2>
+                <BlackSquare />
+                <BlackSquare1 />
+              </FlexRow2>
+            </FlexRow1>
+            <Image4 src={`https://file.rendit.io/n/67PWVh0M2LLJ6vFkM2Xv.svg`} />
+          </Gamepadlight>
+        </Ellipse2>
+        <FlexColumn1>
+          <WhiteFlexColumn>
+            <Text1>E-mail</Text1>
+          </WhiteFlexColumn>
+          <WhiteFlexColumn1>
+            <Text2>Password</Text2>
+          </WhiteFlexColumn1>
+          <FlexColumn2>
+            <WhiteRectangle />
+            <InsertName>Log in</InsertName>
+          </FlexColumn2>
+        </FlexColumn1>
+        <FlexColumn3>
+          <Ellipse3 src={`https://file.rendit.io/n/f4yMF55kwV8Gl7PIfFHV.svg`} />
+          <Ellipse4 src={`https://file.rendit.io/n/8MQJeYFuTw623ryTTMVl.svg`} />
+        </FlexColumn3>
+        <FlexColumn4>
+          <Image5 src={`https://file.rendit.io/n/c0yftxPQtCRmsw8GDLi4.svg`} />
+          <Image6 src={`https://file.rendit.io/n/lqG9xVEK02mGy5dea0Aq.svg`} />
+        </FlexColumn4>
+      </FlexRow>
+      <FlexRow3>
+        <Text3>or</Text3>
+        <Line src={`https://file.rendit.io/n/TSPKsZjTLTELEbzHBr6S.svg`} />
+        <Line src={`https://file.rendit.io/n/TSPKsZjTLTELEbzHBr6S.svg`} />
+      </FlexRow3>
+      <WhiteFlexColumn2>
+        <Text4>Register</Text4>
+      </WhiteFlexColumn2>
+      <Ellipse>
+        <Ellipse1 src={`https://file.rendit.io/n/RhUiufIr2pUse3B3qfCL.svg`} />
+      </Ellipse>
+    </AndroidLargeRootRootRoot>
+  );
+};
+
+const Line = styled.img`
+  width: 62px;
+  height: 1px;
+  position: relative;
+`;
+const AndroidLargeRootRootRoot = styled.div`
+  width: 360px;
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  background-color: #e3e5ef;
+  overflow: hidden;
+`;
+const FlexRow = styled.div`
+  width: 360px;
+  position: relative;
+  gap: 98.5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-self: stretch;
+  align-items: center;
+  margin: 0px 0px 7px 0px;
+  padding: 0px 0px 151px 0px;
+`;
+const Ellipse2 = styled.div`
+  left: 116px;
+  top: 151px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 27px 29px 31px 29px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/sNc0re7AAdn5Gz6gn2Sa.svg");
+`;
+const Gamepadlight = styled.div`
+  width: 35.5px;
+  height: 59.3px;
+  position: relative;
+  gap: 10.7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  padding: 5.33px 17.5px 5.33px 17px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+const Image1 = styled.img`
+  width: 12.7px;
+  height: 18.5px;
+  position: relative;
+  margin: 0px 11.2px 0px 0px;
+`;
+const FlexRow1 = styled.div`
+  height: 12.7px;
+  position: relative;
+  gap: 8.25px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-self: stretch;
+  align-items: center;
+`;
+const FlexColumn = styled.div`
+  width: 1px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0px 5.83px;
+`;
+const Image2 = styled.img`
+  width: 12.7px;
+  height: 1px;
+  left: 0px;
+  top: 5.83px;
+  position: absolute;
+`;
+const Image3 = styled.img`
+  width: 1px;
+  height: 12.7px;
+  position: relative;
+`;
+const FlexRow2 = styled.div`
+  gap: 2.92px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0.5px 0px;
+`;
+const BlackSquare = styled.div`
+  width: 5.83px;
+  height: 5.83px;
+  align-self: flex-end;
+  border-radius: 1px;
+  background-color: #222222;
+`;
+const BlackSquare1 = styled.div`
+  width: 5.83px;
+  height: 5.83px;
+  border-radius: 1px;
+  background-color: #222222;
+`;
+const Image4 = styled.img`
+  width: 53.5px;
+  height: 36px;
+  left: 8.25px;
+  top: 22.8px;
+  position: absolute;
+`;
+const FlexColumn1 = styled.div`
+  height: 185px;
+  left: 18px;
+  top: 305px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 19px 24px 19px 23px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/WjPFWzG0MzLG4qyZdhR1.svg");
+`;
+const WhiteFlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin: 0px 0px 14px 0px;
+  padding: 4px 14px 5px 14px;
+  border-radius: 10px;
+  background-color: #fbfbfc;
+`;
+const Text1 = styled.div`
+  width: 254px;
+  height: 34px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  font-size: 16px;
+  font-family: Sedan;
+`;
+const WhiteFlexColumn1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin: 0px 0px 26px 0px;
+  padding: 8px 92px 7px 10px;
+  border-radius: 10px;
+  background-color: #fbfbfc;
+`;
+const Text2 = styled.div`
+  width: 180px;
+  height: 28px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  font-size: 16px;
+  font-family: Sedan;
+`;
+const FlexColumn2 = styled.div`
+  width: 221px;
+  height: 42px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0px 30px 0px 31px;
+`;
+const WhiteRectangle = styled.div`
+  width: 177px;
+  height: 42px;
+  left: 52px;
+  top: 0px;
+  position: absolute;
+  border-radius: 10px;
+  background-color: rgba(251, 251, 252, 0.5);
+`;
+const InsertName = styled.div`
+  width: 221px;
+  height: 39px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  font-family: Sedan;
+  text-align: center;
+`;
+const FlexColumn3 = styled.div`
+  width: 57px;
+  height: 377px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+const Ellipse3 = styled.img`
+  width: 42px;
+  height: 195px;
+  left: 0px;
+  top: 151px;
+  position: absolute;
+`;
+const Ellipse4 = styled.img`
+  width: 57px;
+  height: 258px;
+  position: relative;
+`;
+const FlexColumn4 = styled.div`
+  width: 174px;
+  height: 377px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 0px 0px 0px 30.7px;
+`;
+const Image5 = styled.img`
+  width: 204px;
+  height: 200px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+`;
+const Image6 = styled.img`
+  height: 200px;
+  position: relative;
+`;
+const FlexRow3 = styled.div`
+  width: 155px;
+  position: relative;
+  gap: 31px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-self: stretch;
+  align-items: center;
+  margin: 0px 0px 7px 0px;
+  padding: 7px 100px 8px 105px;
+`;
+const Text3 = styled.div`
+  width: 64px;
+  height: 16px;
+  left: 150px;
+  top: 0px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  font-family: Sedan;
+  text-align: center;
+`;
+const WhiteFlexColumn2 = styled.div`
+  height: 47px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0px 64px 0px 0px;
+  padding: 0px 17px 0px 16px;
+  border-radius: 10px;
+  background-color: #ffffff;
+`;
+const Text4 = styled.div`
+  width: 187px;
+  height: 41px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  font-family: Sedan;
+  text-align: center;
+`;
+const Ellipse = styled.div`
+  height: 195px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0px 0px 0px 21px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/gELDeNSApKD3egb0Yjnv.svg");
+`;
+const Ellipse1 = styled.img`
+  width: 172px;
+  height: 175px;
+`;
