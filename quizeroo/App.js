25ceadf31388c51a579b5d54bc234b0e91d42542
danielbociat <41957946+danielbociat@users.noreply.github.com>
2023-01-14 17:53:08 +0200

    Added navigation, removed unneccessary buttons

--- quizeroo/App.js
+++ quizeroo/App.js
@@ -2,21 +2,25 @@ import * as React from 'react'
 import { View, Text, StyleSheet ,SafeAreaView} from 'react-native'
 import Login from './screens/LoginScreen';
 import SignUp from './screens/SignUpScreen';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
 const App = () =>{
   return(
-      <SafeAreaView style = {styles.root}>
-        <SignUp/>
-        </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>    
   );
-} ;
+}; 
 
 const styles = StyleSheet.create({
   root:{
     flex:1,
-    
-   
   }
 });
 
--- quizeroo/screens/LoginScreen/Login.js
+++ quizeroo/screens/LoginScreen/Login.js
@@ -4,10 +4,11 @@ import Logo from  '../../Images/Untitled_Artwork.png'
 import CustomInput from '../../components/CustomInput'
 import CustomButton from '../../components/CustomButton'
 
-const Login =() => {
+const Login = ( {navigation} ) => {
     const {height} = useWindowDimensions();
     const [username , setUsername] = useState('');
     const [password , setPassword] = useState('');
+
     const onLoginPressed = () => {
         console.warn("Sign in");
     };
@@ -15,68 +16,41 @@ const Login =() => {
         console.warn("onForgotPasswordPressed");
 
     };
-    
-    const onLoginWithFacebook = () => {
-        console.warn("Facebook");
-    };
-    
-    const onLoginWithGoogle = () => {
-        console.warn("Google");
-    };
-    
     const onSignUpPressed = () => {
-        console.warn("Google");
-    }
-
+        navigation.navigate('SignUp');
+    };
 
     return(
         <ScrollView>
-        <View style={styles.root}>
-            <Image 
-            source = {Logo} 
-            style={[styles.logo  ,
-            {height: height*0.4}]} 
-            resizeMode="contain" />
+            <View style={styles.root}>
+                <Image 
+                source = {Logo} 
+                style={[styles.logo  ,
+                {height: height*0.4}]} 
+                resizeMode="contain" />
 
-            <CustomInput 
-            placeholder= "Username..." 
-            value={username} 
-            setValue={setUsername}/>
+                <CustomInput 
+                placeholder= "Username..." 
+                value={username} 
+                setValue={setUsername}/>
 
-            <CustomInput 
-            placeholder = "Password" 
-            value={password} 
-            setValue={setPassword}/>
-            
-            <CustomButton text="Sign In" onPress={onLoginPressed} />
-            
-            <CustomButton 
-            text="Forgot password ?" 
-            onPress={onForgotPasswordPressed}
-            type = "TERTIARY" />
-            
-            <CustomButton 
-            text="Sign in with Facebook" 
-            onPress={onLoginWithFacebook} 
-            bgColor="#E7EAF4"
-            fgColor="#4765A9"/>
-           
-    
-            <CustomButton 
-            text="Sign in with Google" 
-            onPress={onLoginWithGoogle}
-            bgColor="#FAE9EA"
-            fgColor="#DD4D44"
-             />
+                <CustomInput 
+                placeholder = "Password" 
+                value={password} 
+                setValue={setPassword}/>
+                
+                <CustomButton text="Sign In" onPress={onLoginPressed} />
+                
+                <CustomButton 
+                text="Forgot password ?" 
+                onPress={onForgotPasswordPressed}
+                type = "TERTIARY" />
 
-            <CustomButton 
-            text="Don't have an account? Create one" 
-            onPress={onSignUpPressed}
-            type = "TERTIARY" />
-            
-            
-
-        </View>
+                <CustomButton 
+                text="Don't have an account? Create one" 
+                onPress={onSignUpPressed}
+                type = "TERTIARY" />
+            </View>
         </ScrollView>
     )
 }
@@ -93,4 +67,5 @@ const styles = StyleSheet.create({
         maxHeight: 200,
     }
 })
+
 export default Login;
