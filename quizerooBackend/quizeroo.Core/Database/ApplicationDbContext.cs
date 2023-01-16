215f12a04889e550f3f087ea5d148117e778341e
danielbociat <41957946+danielbociat@users.noreply.github.com>
2023-01-17 00:09:38 +0200

    small change

--- quizerooBackend/quizeroo.Core/Database/ApplicationDbContext.cs
+++ quizerooBackend/quizeroo.Core/Database/ApplicationDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using quizeroo.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace quizeroo.Core.Database
 {
